c6a201e8bc309fab7f4830be66eb6841486dfc7e
Narender Singh <narender2031@gmail.com>
2021-02-11 00:39:58 +0530

    add todo listing

--- src/routes/todo.svelte
+++ src/routes/todo.svelte
@@ -9,10 +9,14 @@
   function handleSubmit() {
     console.log(todoData);
 
-    myTodos.push({...todoData, created_at: new Date(), update_at: new Date()})
+    myTodos = [...myTodos, {...todoData, created_at: new Date(), update_at: new Date()}]
+
+    console.log(myTodos)
     // Clear the value
     todoData = {}
   }
+
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
 </script>
 
@@ -47,6 +51,45 @@
         }
       }
     }
+
+    .todo-list {
+      display: flex;
+      padding: 0.6rem;
+
+      .container {
+        width: 100%;
+        .lists {
+          border: 1px solid #cccc;
+          border-radius: 8px;
+          height: calc(100vh - 400px);
+          width: 100%;
+          padding: 10px;
+          overflow-y: scroll;
+
+          ul {
+            list-style-type: none;
+            padding: 10px;
+          }
+
+          h3 {
+            font-weight: 800;
+          }
+
+          p {
+            margin: 0;
+          }
+
+          .date {
+            font-size: 10px;
+            font-style: italic;
+            color: #cccc;
+            text-align: end;
+            display: flex;
+            justify-content: flex-end;
+          }
+        }
+      }
+    }
   }
 </style>
 
@@ -75,6 +118,23 @@
     </form>
   </div>
   <div class="todo-list">
-    
+    <div class="container">
+      <h4>My Todos</h4>
+      {#if myTodos.length > 0}
+        <div class="lists">
+          <ul>
+            {#each myTodos as todo}
+              <li>
+                <h4>Title: {todo.title}</h4>
+                <p>Descriptopn: {todo.descriptiom}</p>
+                <span class="date">{new Intl.DateTimeFormat('en-US', options).format(todo.created_at)}</span>
+              </li>
+            {/each}
+          </ul>
+        </div>
+      {:else}
+        <p> No Task for the day</p>
+      {/if }
+    </div>
   </div>
 </div>
